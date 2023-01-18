37f09d12457d6ebe5fea11abfd2b6f21f7e080b1
granen32 <granen32@eigene.ai>
2023-01-18 18:45:00 +0900

    보일러 플레이트 세팅

--- src/components/ExpenseItem/index.tsx
+++ src/components/ExpenseItem/index.tsx
@@ -1,7 +1,18 @@
 import React from 'react'
+import * as S from './style'
 
 const ExpenseItem = () => {
-  return <div>ExpenseItem</div>
+  return (
+    <>
+      <S.ExpenseItemWrap>
+        <div>March 28th 2021</div>
+        <div className="description">
+          <h2>Car Insurance</h2>
+          <div className="price">$294.67</div>
+        </div>
+      </S.ExpenseItemWrap>
+    </>
+  )
 }
 
 export default ExpenseItem
