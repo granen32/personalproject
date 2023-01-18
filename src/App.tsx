46012d7bcfbf2b367003fb1764b09a106841ebb3
granen32 <granen32@eigene.ai>
2023-01-18 18:55:03 +0900

    스타일드 컴포넌트 세팅

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,12 @@ import React, { FC } from 'react'
 import ExpenseItem from './components/ExpenseItem'
 import { ThemeProvider } from 'styled-components'
 import { Theme } from 'styles/Theme'
+import { GlobalStyle } from 'styles/globalStyle'
 const App: FC = () => {
   return (
     <>
       <ThemeProvider theme={Theme}>
+        <GlobalStyle />
         <ExpenseItem />
       </ThemeProvider>
     </>
